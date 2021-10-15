a204d8fb2d1cfefa1ffe9b8e562b89e0ac0c6ff2
unknown <deepakumar50136@gmail.com>
2021-10-15 21:05:39 +0530

    first flask project

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template 
+
+    
+app = Flask(__name__) 
+    
+@app.route('/') 
+def Content(): 
+    with open('file1.txt', 'r') as f: 
+        text = f.read()
+        print(text)
+        return render_template('content.html', text=text)
+@app.route('/file2') 
+def Content2(): 
+    with open('file2.txt', 'r') as f: 
+        text = f.read()
+        print(text)
+        return render_template('content.html', text=text)
+
+@app.route('/file3') 
+def Content3(): 
+    with open('file3.txt', 'r') as f: 
+        text = f.read()
+        print(text)
+        return render_template('content.html', text=text)
+
+@app.route('/file4') 
+def Content4(): 
+    with open('file4.txt', errors="ignore") as f: 
+        text = f.read()
+        print(text)
+        return render_template('content.html', text=text)
+
+app.run()
