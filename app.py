5d5be3e0329fdff7de85a15847e20959abefb94e
unknown <deepakumar50136@gmail.com>
2021-10-16 13:36:22 +0530

    url fixing

--- app.py
+++ app.py
@@ -4,29 +4,47 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route(f"/<id>")
+@app.route("/", defaults={"id": None})
+@app.route("/<id>")
 def Content(id):
 
     with open("file1.txt", "r") as f1, open("file1.txt", "r") as f2:
-
         text = f1.readlines()
         full_text = f2.read()
+        if id != None:
 
-        for text in text:
-            if id in text[0:4]:
-                print(True)
-                text = text
-                break
+            for text in text:
+                if id in text[0:4]:
+                    print(True)
+                    text = text
+                    return render_template("content.html", text=text)
+                    break
+        else:
+            return render_template("content.html", text=full_text)
 
-    return render_template("content.html", text=text)
 
+@app.route("/file2", defaults={"id": None})
+@app.route("/file2/<id>")
+def Content2(id):
+    with open("file2.txt", "r") as f1, open("file2.txt", "r") as f2:
+        text = f1.readlines()
+        full_text = f2.read()
+        if id != None:
+   
 
-@app.route("/file2")
-def Content2():
-    with open("file2.txt", "r") as f:
-        text = f.read()
+            for text in text:
 
-        return render_template("content.html", text=text)
+                if id in text[0:4]:
+                    print(text[0:4])
+                    print("id:", id)
+                    print(True)
+                    text = text
+
+                    return render_template("content.html", text=text)
+                    print("break")
+
+        else:
+            return render_template("content.html", text=full_text)
 
 
 @app.route("/file3")
@@ -38,11 +56,11 @@ def Content3():
 
 
 @app.route("/file4")
-def Content4():
+def Content4(id):
     with open("file4.txt", errors="ignore") as f:
         text = f.read()
 
         return render_template("content.html", text=text)
 
 
-app.run(debug=False)
+app.run(debug=True)
